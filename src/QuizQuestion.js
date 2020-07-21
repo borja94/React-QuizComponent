44b54249b32404e8d880015ccf827d6f7f089136
Borja Guzmán López <bguzman@plainconcepts.com>
2020-07-21 19:37:37 +0200

    Displaying Quiz Questions and Handling Events

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,6 +4,13 @@ import QuizQuestionButton from "./QuizQuestionButton";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
+    this.handleClick.bind(this);
+  }
+
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
   }
   render() {
     return (
@@ -13,9 +20,15 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton
-              button_text={this.props.quiz_question.answer_options[0]}
-            />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => (
+                <QuizQuestionButton
+                  button_text={answer_option}
+                  key={index}
+                  clickHandler={this.handleClick.bind(this)}
+                />
+              )
+            )}
           </ul>
         </section>
       </main>
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,11 +4,15 @@ class QuizQuestionButton extends Component {
   constructor(props) {
     super(props);
   }
+
+  handleClick() {
+    this.props.clickHandler(this.props.button_text);
+  }
   render() {
     return (
-      <li>
-        <button>{this.props.button_text}</button>
-      </li>
+      <button onClick={this.handleClick.bind(this)}>
+        {this.props.button_text}
+      </button>
     );
   }
 }
